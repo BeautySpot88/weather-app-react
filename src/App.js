b64097b23ba5bd24769b86da37d6f10402b3a122
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-02 11:57:12 +0100

    updated netlify link

--- src/App.js
+++ src/App.js
@@ -13,11 +13,21 @@ export default function App() {
       </div>
       <footer>
         Created by Ebele and is{" "}
-        <a href="https://github.com/BeautySpot88/weather-app-react">
+        <a
+          href="https://github.com/BeautySpot88/weather-app-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           open-sourced on GitHub
         </a>{" "}
         and hosted on{" "}
-        <a href="https://github.com/BeautySpot88/weather-app-react">Netlify</a>
+        <a
+          href="https://agitated-pike-3fe7d3.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );
