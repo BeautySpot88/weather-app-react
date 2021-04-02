5c4f75de364502d91b7f419df283c61c5829eebe
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-02 23:02:08 +0100

    Added daily forecast layout

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -1,5 +1,20 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./DailyWeather.css";
 
 export default function DailyWeather(props) {
-  return "Daily Forecast";
+  return (
+    <div className="DailyWeather">
+      <div className="row">
+        <div className="col">
+          <div className="DailyWeather-day">Thursday</div>
+          <WeatherIcon code="01d" />
+          <div className="DailyWeather-temperature">
+            <span className="DailyWeather-max">19°C</span>
+            <span className="DailyWeather-min">10°C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
