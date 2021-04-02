7f37c67fe50d5d6505e007aad3fc57e588517635
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-02 11:48:43 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ export default function App() {
           </main>
         </div>
       </div>
-      <footer>Created by Ebele</footer>
+      <footer>
+        Created by Ebele and is{" "}
+        <a href="https://github.com/BeautySpot88/weather-app-react">
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://github.com/BeautySpot88/weather-app-react">Netlify</a>
+      </footer>
     </div>
   );
 }
